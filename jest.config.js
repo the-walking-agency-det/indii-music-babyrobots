fb18e9f81504ae5691b2cfa8acd9e20b9a627800
thewalkeragency <williamexpressway@gmail.com>
2025-07-18 09:46:24 -0400

    chore: Update configuration and environment files

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,13 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['<rootDir>/tests/setup.js'],
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transform: {
-    '^.+\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }],
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '\.pnp\.[^/]+$',
-  ],
-  moduleNameMapper: {
-    '\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
-  },
   testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
-    '**/?(*.)+(spec|test).[tj]s?(x)',
+    '**/__tests__/**/*.test.js',
+    '**/__tests__/**/*.test.jsx',
+    '**/__tests__/**/*.test.ts',
+    '**/__tests__/**/*.test.tsx',
   ],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/.next/',
-    '/coverage/',
-    '/__tests__/',
-    '/public/',
-  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }],
+  },
 };
